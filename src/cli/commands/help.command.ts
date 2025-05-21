bed6861c9052c3e3179f84ede085dc5d57545694
d.korbut <d.korbut@vk.team>
2025-05-21 18:48:40 +0300

    module1-task2: Добавляет команду --help для CLI
    Добавляет новый класс HelpCommand, который реализует интерфейс Command и выводит справочную информацию о доступных командах CLI.

---
+++ src/cli/commands/help.command.ts
@@ -0,0 +1,26 @@
+import {Command} from './command.interface.js';
+
+export class HelpCommand implements Command {
+  private readonly commandName: string = '--help';
+
+  getName(): string {
+    return this.commandName;
+  }
+
+  execute(..._params: string[]): void {
+    console.info(`
+      Программа для подготовки данных для REST API сервера.
+
+      Пример: cli.js --<command> [--arguments]
+
+      Команды:
+
+       --version:                   # выводит номер версии
+       --help:                      # печатает этот текст
+       --import <path>:             # импортирует данные из TSV
+       --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+    `);
+  }
+}
+
+new HelpCommand().execute();
