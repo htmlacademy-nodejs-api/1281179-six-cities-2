ff26f944892fed3da7f9fc728d3a65b0717d8eed
d.korbut <d.korbut@vk.team>
2025-05-22 15:47:52 +0300

    module1-task2: Добавляет парсинг аргументов и обработку команд
    Добавляет метод parse в класс CommandParser для парсинга аргументов командной строки.
    Добавляет конструктор, метод getCommand и processCommand в класс CLIApplication.
    Добавляет метод getDefaultCommand для получения команды по умолчанию.
    Обновляет логику обработки команд в методе processCommand.

--- src/cli/cli-application.ts
+++ src/cli/cli-application.ts
@@ -1,9 +1,19 @@
 import {Command} from './commands/command.interface.js';
+import {CommandParser} from './command-parser.js';
 
 type CommandCollection = Record<string, Command>
 
 export class CLIApplication {
   private commands: CommandCollection = {};
+
+  constructor(
+    private readonly defaultCommand: string = '--help'
+  ) {
+  }
+
+  public getCommand(commandName: string) {
+    return this.commands[commandName] || this.getDefaultCommand();
+  }
 
   public registerCommands(commands: Command[]) {
     commands.forEach((command) => {
@@ -14,5 +24,23 @@ export class CLIApplication {
 
       this.commands[commandName] = command;
     });
+  }
+
+  public processCommand(params: string[]): void {
+    const parsedCommands = CommandParser.parse(params);
+    const commandNames = Object.keys(parsedCommands);
+    commandNames.forEach((commandName) => {
+      const command = this.getCommand(commandName);
+      const commandParams = parsedCommands[commandName] || [];
+      command.execute(...commandParams);
+    });
+  }
+
+  private getDefaultCommand(): never | Command {
+    if (!this.commands[this.defaultCommand]) {
+      throw new Error(`The default command ${this.defaultCommand} is not registered`);
+    }
+
+    return this.commands[this.defaultCommand];
   }
 }
--- src/cli/command-parser.ts
+++ src/cli/command-parser.ts
@@ -1,6 +1,17 @@
 type ParsedCommand = Record<string, string[]>
 
 export class CommandParser {
+  /**
+   * Парсит массив аргументов командной строки в структурированный объект.
+   * Каждый флаг команды (начинающийся с "--") является ключом в возвращаемом объекте.
+   * А значения, связанные с ключом, это массив переданных аргументов.
+   * @param cliArguments
+   * @returns Объект, где ключ - флаг команды, а значение - массив аргументов, следующий за этим флагом.
+   * @example
+   * const args = ['--import', './mocks/offers.tsv'];
+   * const parsed = CommandParser.parse(args); // parsed будет соответствовать {'--import': ['./mocks/offers.tsv']}
+   *
+   */
   static parse(cliArguments: string[]) {
     const parsedCommand: ParsedCommand = {};
     let currentCommand = '';
