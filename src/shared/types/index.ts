0ced9326e096c1e8373f51ead3d8573a041ef60b
d.korbut <d.korbut@vk.team>
2025-05-23 14:53:31 +0300

    module1-task2: Добавляет типы данных для предложений
    Добавляет новые типы данных: Offer, Property и Convenience, необходимые для описания предложений в приложении.

---
+++ src/shared/types/index.ts
@@ -0,0 +1,3 @@
+export type {Offer} from './offer.type.js';
+export type {Property} from './property.type.js';
+export type {Convenience} from './conveniences.type.js';
---
+++ src/shared/types/property.type.ts
@@ -0,0 +1,6 @@
+export enum Property {
+  APARTMENT = 'apartment',
+  HOUSE = 'house',
+  ROOM = 'room',
+  HOTEL = 'hotel'
+}
