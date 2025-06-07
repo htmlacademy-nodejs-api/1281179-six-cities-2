25c6a43ef09457a6d7614deaa109c481b2d0e0fb
d.korbut <d.korbut@vk.team>
2025-06-07 10:03:25 +0300

    module3-task1: Добавляет транспорт в pino для записи логов в файл

--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,11 +1,28 @@
-import { Logger as PinoInstanse, pino } from 'pino';
+import { Logger as PinoInstanse, pino, transport } from 'pino';
 import { Logger } from './logger.interface.js';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstanse;
 
   constructor() {
-    this.logger = pino();
+    // Получаем путь к текущему файлу
+    const __fileName = fileURLToPath(import.meta.url);
+
+    // Получаем директорию текущего файла
+    const __dirName = dirname(__fileName);
+
+    // Получаем путь к файлу логов
+    const destination = resolve(__dirName, '../../../../', 'logs/app.log');
+    const pinoTransport = transport({
+      target: 'pino/file',
+      options: {
+        destination
+      }
+    });
+
+    this.logger = pino({}, pinoTransport);
   }
 
   info(message: string, ...args: unknown[]): void {
