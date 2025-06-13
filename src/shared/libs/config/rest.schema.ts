c77665a7ca6309745c97e6554821c88b35ab3abb
d.korbut <d.korbut@vk.team>
2025-06-13 15:19:08 +0300

    module4-task1: Добавляет `mongoose` для работы с `mangodb`

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -5,6 +5,9 @@ export type RestSchema = {
   PORT: string;
   SALT: string;
   DB_HOST: string;
+  DB_USERNAME: string;
+  DB_PASSWORD: string
+  DB_PORT: string;
 }
 
 convict.addFormats(validator);
@@ -27,5 +30,23 @@ export const configRestSchema = convict<RestSchema>({
     format: 'ipaddress',
     env: 'DB_HOST',
     default: '127.0.0.1'
+  },
+  DB_USERNAME: {
+    doc: 'Username for connecting to the database',
+    format: String,
+    env: 'DB_USERNAME',
+    default: null
+  },
+  DB_PASSWORD: {
+    doc: 'Password for connecting to the database',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null
+  },
+  DB_PORT: {
+    doc: 'Port for connecting to the database',
+    format: 'port',
+    env: 'DB_PORT',
+    default: '27017'
   }
 });
