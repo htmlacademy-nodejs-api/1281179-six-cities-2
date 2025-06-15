cab28151f887bd3c31c4a80e9af8ba595eaee93a
d.korbut <d.korbut@vk.team>
2025-06-15 17:30:40 +0300

    module4-task2: Добавит определение модели

--- src/shared/modules/users/user.entity.ts
+++ src/shared/modules/users/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User, UserType } from '../../types/user.type.js';
 
 export class UserEntity implements User {
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: true })
   public name: string;
+
+  @prop({ required: false, default: '' })
   public photo?: string;
+
+  @prop({ required: true })
   public password: string;
+
+  @prop({ required: true })
   public userType: UserType;
 }
+
+export const UserModel = getModelForClass(UserEntity);
