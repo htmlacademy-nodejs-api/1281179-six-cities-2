1a22dafdd13b4113b6d6c9be7610504f9a36ee17
d.korbut <d.korbut@vk.team>
2025-07-29 19:38:17 +0300

    Добавлен абстрактный класс BaseController для упрощения работы с маршрутизацией и отправкой ответов в REST API. Обновлен интерфейс Controller для поддержки параметра contentType в методе send. Изменены значения методов перечисления HttpMethod на строчные.

--- src/shared/libs/rest/controller/controller.interface.ts
+++ src/shared/libs/rest/controller/controller.interface.ts
@@ -4,7 +4,7 @@ import { Route } from '../types/route.interface.js';
 export interface Controller {
   readonly router: Router;
   addRoute(route: Route): void;
-  send<T>(res: Response, statusCode: number, data: T): void;
+  send<T>(res: Response, statusCode: number, data: T, contentType?: string): void;
   ok<T>(res: Response, data: T): void;
   created<T>(res: Response, data: T): void;
   noContent<T>(res: Response, data: T): void;
