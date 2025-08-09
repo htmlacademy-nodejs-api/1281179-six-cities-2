4b3cbe7bcd9209d48fe92c536b6bad5c3f0fd601
d.korbut <d.korbut@vk.team>
2025-08-09 13:24:54 +0300

    Добавлен экспорт интерфейса ExceptionFilter из модуля rest, что улучшает обработку исключений в приложении.

--- src/shared/libs/rest/index.ts
+++ src/shared/libs/rest/index.ts
@@ -1,3 +1,4 @@
 export * from './types/http-method.enum.js';
 export * from './types/route.interface.js';
 export * from './controller/controller.interface.js';
+export * from './exception-filter/exception-filter.interface.js';
