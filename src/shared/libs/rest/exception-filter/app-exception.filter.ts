c98205b0445d4af68d8ff6a82ac66a3aedb24184
d.korbut <d.korbut@vk.team>
2025-08-09 13:54:26 +0300

    Обновлен фильтр исключений AppExceptionFilter для улучшенной обработки ошибок. Добавлены методы для обработки HTTP ошибок и других ошибок с использованием класса HttpError и функции createErrorObject, что позволяет более гибко управлять информацией об ошибках и улучшает логирование.

--- src/shared/libs/rest/exception-filter/app-exception.filter.ts
+++ src/shared/libs/rest/exception-filter/app-exception.filter.ts
@@ -4,6 +4,8 @@ import { Components } from '../../../types/components.enum.js';
 import { Request, Response, NextFunction } from 'express';
 import { Logger } from '../../../libs/logger/logger.interface.js';
 import { StatusCodes } from 'http-status-codes';
+import { HttpError } from '../errors/http-error.js';
+import { createErrorObject } from '../../../helpers/common.js';
 
 @injectable()
 export class AppExceptionFilter implements ExceptionFilter {
@@ -13,12 +15,24 @@ export class AppExceptionFilter implements ExceptionFilter {
   ) {}
 
   catch(err: Error, _req: Request, res: Response, _next: NextFunction): void {
-    this.logger.error(`[AppExceptionFilter] ${err.message}`, err);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({
-        error: err.message,
-      });
+    if (err instanceof HttpError) {
+      this.handleHttpError(err, _req, res, _next);
+    } else {
+      this.handleOtherError(err, _req, res, _next);
+    }
   }
 
+  private handleHttpError(error: HttpError, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(`[${error.detail}]: ${error.httpStatusCode} - ${error.message}`, error);
+    res
+      .status(error.httpStatusCode)
+      .json(createErrorObject(error.message, error.detail));
+  }
+
+  private handleOtherError(error: Error, _req: Request, res: Response, _next: NextFunction) {
+    this.logger.error(error.message, error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(createErrorObject(error.message));
+  }
 }
