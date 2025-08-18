2a7d3cd05f92667c4258e19bea9c6375649ff31f
d.korbut <d.korbut@vk.team>
2025-08-18 18:33:44 +0300

    Добавлен новый промежуточный обработчик DocumentExistMiddleware для проверки существования документа по заданному идентификатору. Также обновлен индексный файл для экспорта нового обработчика и добавлен интерфейс DocumentExist для определения метода проверки существования документа. Это улучшает обработку ошибок и валидацию данных в приложении.

--- src/apps/rest/index.ts
+++ src/apps/rest/index.ts
@@ -2,3 +2,4 @@ export * from './rest.application.js';
 export * from './middleware/middleware.interface.js';
 export * from './middleware/validate-objectId.middleware.js';
 export * from './middleware/validate-dto.middleware.js';
+export * from './middleware/document-exist.middleware.js';
