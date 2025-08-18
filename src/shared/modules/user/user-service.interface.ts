16e5e68842831c2a0cb157116719e8086c47e65e
d.korbut <d.korbut@vk.team>
2025-08-18 18:57:29 +0300

    Добавлены методы deleteById, updateById и exists в DefaultUserService для управления пользователями. Обновлен интерфейс UserService для включения новых методов. В UserController добавлены маршруты для получения, обновления и удаления пользователей с использованием DocumentExistMiddleware для проверки существования документа. Эти изменения улучшают управление пользователями и обработку ошибок в приложении.

--- src/shared/modules/user/user-service.interface.ts
+++ src/shared/modules/user/user-service.interface.ts
@@ -1,12 +1,15 @@
 import { DocumentType } from '@typegoose/typegoose';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UserEntity } from './user.entity.js';
+import { DocumentExist } from '../../types/document-exist.interface.js';
 
-export interface UserService {
+export interface UserService extends DocumentExist {
   create(dto: CreateUserDto, salt:string): Promise<DocumentType<UserEntity>>;
   findByEmail(email: string): Promise<DocumentType<UserEntity> | null>;
   findOrCreate(dto: CreateUserDto, salt: string): Promise<DocumentType<UserEntity>>;
   findUserById(id: string): Promise<DocumentType<UserEntity> | null>;
   findAll: () => Promise<DocumentType<UserEntity>[]> | null;
   comparePassword(password: string, hash: string, salt: string): Promise<boolean>;
+  deleteById(id: string): Promise<number>;
+  updateById(id: string, dto: CreateUserDto): Promise<DocumentType<UserEntity> | null>;
 }
