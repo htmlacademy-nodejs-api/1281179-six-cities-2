54ad8bb1d6272a9b77a78ce8c78e48bb036f391b
d.korbut <d.korbut@vk.team>
2025-08-18 18:47:38 +0300

    Обновлен интерфейс OfferService для наследования от DocumentExist, добавлен новый метод delete в OfferController с соответствующими промежуточными обработчиками для проверки существования документа. Это улучшает обработку запросов и управление ошибками при удалении предложений.

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -2,8 +2,9 @@ import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { UpdateOfferDto } from './dto/update-offer.dto.js';
 import { DocumentType } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
+import { DocumentExist } from '../../types/document-exist.interface.js';
 
-export interface OfferService {
+export interface OfferService extends DocumentExist {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findByOfferId(offerId: string): Promise<DocumentType<OfferEntity> | null>;
   findByCityId(cityId: string): Promise<DocumentType<OfferEntity>[]>;
